ca7765205ab818735f08d49091e43a1054c2d727
mikkeldamm <mikkeldamm@hotmail.com>
2015-03-16 10:48:55 +0100

    Added second acceptance criteria for exercise 2

---
+++ CodeKatas/CodeKata.Calculator.Exercise2.Tests/StringCalculatorTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKata.Calculator.Exercise2.Tests
+{
+    //An empty string returns zero
+    //A single number returns the value
+    //Two numbers, comma delimited, returns the sum
+    //Two numbers, newline delimited, returns the sum
+    //Three numbers, delimited either way, returns the sum
+    //Negative numbers throw an exception
+    //Numbers greater than 1000 are ignored
+    //A single char delimiter can be defined on the first line (e.g. //# for a ‘#’ as the delimiter)
+    //A multi char delimiter can be defined on the first line (e.g. //[###] for ‘###’ as the delimiter)
+
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private StringCalculator _sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _sut = new StringCalculator();
+        }
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_EmptyStringInput_ReturnsZero()
+        {
+            // Arrange
+            var expected = 0;
+            var input = "";
+
+            // Act
+            var output = _sut.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_SingleNumberInput_ReturnsTheValue()
+        {
+            // Arrange
+            var expected = 1;
+            var input = "1";
+
+            // Act
+            var output = _sut.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, output);
+        }
+    }
+
+    internal class StringCalculator
+    {
+        public int Calculate(string input)
+        {
+            int num;
+
+            int.TryParse(input, out num);
+
+            return num;
+        }
+    }
+}

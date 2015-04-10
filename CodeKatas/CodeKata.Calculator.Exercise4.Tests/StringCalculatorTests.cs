8143f443b5caa579573af4c0a0a50eb2965fd53e
mikkeldamm <mikkeldamm@hotmail.com>
2015-04-10 13:50:45 +0200

    Created tests for exercise 5

---
+++ CodeKatas/CodeKata.Calculator.Exercise4.Tests/StringCalculatorTests.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKata.Calculator.Exercise4.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private StringCalculator _stringCalculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        //--An empty string returns zero
+        //--A single number returns the value
+        //--Two numbers, comma delimited, returns the sum
+        //--Two numbers, newline delimited, returns the sum
+        //--Three numbers, delimited either way, returns the sum
+        //--Negative numbers throw an exception
+        //--Numbers greater than 1000 are ignored
+        //A single char delimiter can be defined on the first line (e.g. //# for a ‘#’ as the delimiter)
+        //A multi char delimiter can be defined on the first line (e.g. //[###] for ‘###’ as the delimiter)
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_EmptyStringInput_ReturnsZero()
+        {
+            // Arrange
+            var expected = 0;
+            var input = "";
+
+            // Act
+            var result = _stringCalculator.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_SingleNumberInput_ReturnsParsedNumber()
+        {
+            // Arrange
+            var expected = 2;
+            var input = "2";
+
+            // Act
+            var result = _stringCalculator.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_TwoNumbersCommaDelimited_ReturnsSumOfNumbers()
+        {
+            // Arrange
+            var expected = 8;
+            var input = "3,5";
+
+            // Act
+            var result = _stringCalculator.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_TwoNumbersNewlineDelimited_ReturnsSumOfNumbers()
+        {
+            // Arrange
+            var expected = 8;
+            var input = @"3\n5";
+
+            // Act
+            var result = _stringCalculator.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_ThreeNumbersDelimitedEitherWay_ReturnsSumOfNumbers()
+        {
+            // Arrange
+            var expected = 13;
+            var input = @"3,8\n2";
+
+            // Act
+            var result = _stringCalculator.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod, TestCategory("CI"), ExpectedException(typeof(ArgumentException))]
+        public void Calculate_NegativeNumberExists_ThrowsArgumentException()
+        {
+            // Arrange
+            var input = "-4,2";
+
+            // Act
+            var result = _stringCalculator.Calculate(input);
+        }
+
+        [TestMethod, TestCategory("CI")]
+        public void Calculate_HighNumberExists_IgnoresHighNumber()
+        {
+            // Arrange
+            var expected = 8;
+            var input = "3,1000,5";
+
+            // Act
+            var result = _stringCalculator.Calculate(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    internal class StringCalculator
+    {
+        private readonly string[] _separators = { ",", @"\n" };
+
+        public int Calculate(string input)
+        {
+            var digits = input.Split(_separators, StringSplitOptions.None).Select(ParseIntegerAndReturnZeroIfEmpty).ToList();
+
+            ThrowArgumentExceptionIfNegativeNubmerExists(digits);
+
+            RemoveDigitsHigherThanThousand(digits);
+
+            return digits.Sum();
+        }
+
+        private static int ParseIntegerAndReturnZeroIfEmpty(string input)
+        {
+            int parsedInt;
+            int.TryParse(input, out parsedInt);
+            return parsedInt;
+        }
+
+        private static void ThrowArgumentExceptionIfNegativeNubmerExists(IEnumerable<int> digits)
+        {
+            if (digits.Any(d => d < 0))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private static void RemoveDigitsHigherThanThousand(List<int> digits)
+        {
+            digits.RemoveAll(d => d > 999);
+        }
+    }
+}
---
+++ CodeKatas/CodeKata.Calculator.Exercise5.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeKata.Calculator.Exercise5.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DFDS A/S")]
+[assembly: AssemblyProduct("CodeKata.Calculator.Exercise5.Tests")]
+[assembly: AssemblyCopyright("Copyright © DFDS A/S 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f2726d3f-30cc-4cb7-a4e0-341730600dc8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
